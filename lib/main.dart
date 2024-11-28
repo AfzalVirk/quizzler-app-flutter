4da5dbdbfd7e87f444c10ca9d93033f72f77fbcb
AfzalVirk <vafzal078@gmail.com>
2024-11-28 15:25:34 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'question.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+  /*List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+   Question q1 = Question(
+     q: 'You can lead a cow down stairs but not up stairs.', a: false);
+
+  List<bool> answers = [false, true, true];*/
+
+  List<Question> questionbank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
+  int questionnum = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionbank[questionnum].questiontext,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: MaterialButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'true',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                bool correctanswer = questionbank[questionnum].questionanswer;
+                if (correctanswer == true) {
+                  print('u got it right');
+                } else {
+                  print('u got it wrong');
+                }
+                setState(() {
+                  questionnum++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: MaterialButton(
+              textColor: Colors.white,
+              color: Colors.red,
+              child: Text(
+                'false',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                bool correctanswer = questionbank[questionnum].questionanswer;
+                if (correctanswer == false) {
+                  print('u got it right');
+                } else {
+                  print('u got it wrong');
+                }
+                setState(() {
+                  questionnum++;
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scorekeeper,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questiontext = '';
+  bool questionanswer = true;
+
+  Question({required String q, required bool a}) {
+    questiontext = q;
+    questionanswer = a;
+  }
+}
